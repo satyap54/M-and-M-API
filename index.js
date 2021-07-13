fedc3d0f4510a42def345cdbb4aa1ad82f8ee079
Satyabrat Panda <satyabratp.m.2000@gmail.com>
2021-07-13 13:29:13 +0530

    created dummy song post

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const routes = require("./routes")
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require("./routes");
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const Song = require('./models/Song');
+const { create } = require('./models/Song');
 
 try{
   mongoose.connect(
@@ -22,7 +25,17 @@ try{
 
 app.use(routes);
 app.use(cors());
+app.use(bodyParser.json());
 
+app.post("/api/song/add", 
+  (req, res)=>{
+    Song.create(req.body, (err, data)=>{
+      if(err)
+        return console.error(err);
+      res.json(data);
+    })   
+  }
+)
 
 const listener = app.listen(process.env.PORT || 8000, ()=>{
   console.log(`App is listening on port ${listener.address().port}`);
--- models/Song.js
+++ models/Song.js
@@ -6,7 +6,8 @@ const songSchema = new Schema(
     name : { type : String, required : true },
     song_url : { type : String, required : true },
     knox_hill_breakdown : { type : String, required : false },
-    tags : { type : [String], required : true }
+    tags : { type : [String], required : true },
+    persona : { type : String, enum : ["B-Rabbit", "Slim Shady", "Eminem", "Marshall Mathers"], required : false}
   },
   { timestamps : true }
 )
